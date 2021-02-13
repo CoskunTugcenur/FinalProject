211758d38cb41eb05c79cf7c05e21d34cedd0702
TugcenurC <coskuntugcenur@gmail.com>
2021-02-14 00:02:19 +0300

    webAPI added

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -33,6 +33,10 @@ namespace Business.Concrete
 
         public IDataResult<List<Product>> GetAll()
         {
+            if (DateTime.Now.Hour==1)
+            {
+                return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
+            }
             //iş kodları
             return new SuccessDataResult<List<Product>>(_IProduct.GetAll(),Messages.ProductListedMessage);
         }
